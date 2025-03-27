171105c0aacb0cd50e2882fda8025cf78e2f2abb
fadi-bakour <fadi.bakour101@gmail.com>
2025-03-28 03:41:11 +0800

    add homepage

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Match the repository name
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react()],
