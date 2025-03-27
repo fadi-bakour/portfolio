0cfcee324b6249871596e2a9e119c834b2706f68
fadi-bakour <fadi.bakour101@gmail.com>
2025-03-28 02:43:34 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio', // Replace with your actual GitHub repo name
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react()],
+    base: '/portfolio',
 })
