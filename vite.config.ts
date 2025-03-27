3feff9f4c85d7fb7c001d7226bb709b6f3f8ca70
fadi-bakour <fadi.bakour101@gmail.com>
2025-03-28 01:31:28 +0800

    add gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'fadi-bakour', // Replace with your actual GitHub repo name
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react()],
